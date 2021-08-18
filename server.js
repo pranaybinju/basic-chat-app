509647ca4efddcef9eae46ae4d621320217f343c
PKB2810 <pranay@kiprosh.com>
2021-08-18 19:06:18 +0530

    adding dynamodb, lambda function, and api update

--- server.js
+++ server.js
@@ -10,19 +10,20 @@ app.get("/", async function (req, res) {
 });
 
 app.get("/sendMessage", function (req, res, next) {
-  fetch(" https://t0rlh3a3bj.execute-api.ap-south-1.amazonaws.com/stage")
+  fetch("https://pquco5qmd3.execute-api.ap-south-1.amazonaws.com/items")
     .then((message) => {
       return message.json();
     })
     .then((parsedMsg) => {
-      sendMiddleware(req, res, next, parsedMsg);
+      console.log(parsedMsg);
+      sendMiddleware(req, res, next, parsedMsg.Items[0].message);
     });
   res.send("sent");
 });
 
 app.get("/receiveMessage", async function (req, res, next) {
   const message = await fetch(
-    " https://t0rlh3a3bj.execute-api.ap-south-1.amazonaws.com/stage"
+    "https://pquco5qmd3.execute-api.ap-south-1.amazonaws.com/items"
   );
   const parsedMsg = await message.json();
   receiveMiddleware(req, res, next);
